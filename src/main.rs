fe76cb8905484ce0b85387d6d99efe382f3f5f7c
korintje <t.hosomi1410@gmail.com>
2021-08-15 01:06:10 +0900

    update to the latest

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-#![forbid(unsafe_code)]
-#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
-#![warn(clippy::all, rust_2018_idioms)]
+mod operation;
+use operation::Operation;
+use core::cell::{Cell, RefCell};
 
-// When compiling natively:
-#[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    let app = herschel::TemplateApp::default();
-    let native_options = eframe::NativeOptions::default();
-    eframe::run_native(Box::new(app), native_options);
+
+  let w1 = vec![0.0, 1.0, 2.0, 3.0];
+  let t1 = vec![2.0, 3.5, 4.2, 9.0];
+  let w2 = vec![0.0, 1.0, 2.0, 3.0];
+  let t2 = vec![3.0, 1.5, 4.7, 6.1];
+
+  let op1 = operation::Generate{freq: RefCell::new(w1), trans: RefCell::new(t1)};
+  let op2 = operation::Generate{freq: RefCell::new(w2), trans: RefCell::new(t2)};
+  let op3 = operation::Add{augend: &op1, addend: &op2, coeff: Cell::new(1.0)};
+  let op4 = operation::Add{augend: &op3, addend: &op2, coeff: Cell::new(-2.0)};
+
+  println!("Trans: {:?}", op4.get_spectrum().trans);
+
+  op3.coeff.set(-1.0);
+  println!("Trans: {:?}", op4.get_spectrum().trans);
+
+  op1.trans.replace(vec![20.0, 23.5, 40.2, 9.0]);
+  println!("Trans: {:?}", op4.get_spectrum().trans);
+
 }
